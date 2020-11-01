adf5435534e72027c349da65c3158d90eb5cbcd3
E.Demidovskaia <violet7693@gmail.com>
2020-11-01 14:48:43 +0300

    feat: add solution for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options === undefined) {
+    return str;
+  } else {
+    options.repeatTimes = (options.repeatTimes === undefined) ? options.repeatTimes = 1 : options.repeatTimes = options.repeatTimes;
+    options.separator = (options.separator === undefined) ? options.separator = '+' : options.separator = options.separator;
+    options.additionSeparator = (options.additionSeparator === undefined) ? options.additionSeparator = '|' : options.additionSeparator = options.additionSeparator;
+    options.additionRepeatTimes = (options.additionRepeatTimes === undefined) ? options.additionRepeatTimes = 1 : options.additionRepeatTimes = options.additionRepeatTimes;
+    options.addition = (options.addition === undefined) ? options.addition = '' : options.addition = options.addition;
+
+    let repeatablePartOfAddition = (options.additionSeparator + options.addition).repeat(options.additionRepeatTimes - 1);
+    let fullAddition = options.addition + repeatablePartOfAddition;  
+
+    let fragmentBasic = options.separator + str + fullAddition;
+    let fragmentFirst = str + fullAddition;
+    const result = fragmentFirst + fragmentBasic.repeat(options.repeatTimes - 1);
+    return result;
+  }
 };
   
